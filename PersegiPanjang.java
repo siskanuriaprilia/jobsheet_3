66165d0b5ef4e7b07fcff3f935fbf1087c677d62
siskanuriaprilia <aprilias878@gmail.com>
2024-02-28 13:29:30 +0700

    percobaan 2 modifikasi 2

--- PersegiPanjang.java
+++ PersegiPanjang.java
@@ -1,11 +1,27 @@
 import java.util.Scanner;
-    public class PersegiPanjang {
+public class PersegiPanjang {
     public int panjang;
     public int lebar;
+
+    public void cetakInfo() 
+    {
+        System.out.println("Panjang: " + panjang + ", Lebar: " + lebar);
+    }
 
     public static void main(String[] args) 
     {
         Scanner sc = new Scanner(System.in);
+        
+        // Membuat array of PersegiPanjang dengan 100 elemen
+        PersegiPanjang[] myArrayOfPersegiPanjang = new PersegiPanjang[100];
+        
+        // Inisialisasi setiap elemen dalam array
+        for (int i = 0; i < myArrayOfPersegiPanjang.length; i++) 
+        {
+            myArrayOfPersegiPanjang[i] = new PersegiPanjang();
+        }
+
+        // Penggunaan input untuk inisialisasi dan cetak informasi persegi panjang
         PersegiPanjang[] arrayOfPersegiPanjang = new PersegiPanjang[3];
 
         for (int i = 0; i < 3; i++) 
@@ -19,10 +35,17 @@ import java.util.Scanner;
             arrayOfPersegiPanjang[i].lebar = sc.nextInt();
         }
 
+        // Akses elemen ke-5 dari array dan atur properti panjangnya
+        myArrayOfPersegiPanjang[5].panjang = 20;
+
+        // Cetak informasi tentang persegi panjang ke-5
+        System.out.println("Panjang persegi panjang ke-5: " + myArrayOfPersegiPanjang[5].panjang);
+
+        // Cetak informasi tentang persegi panjang yang dimasukkan pengguna
         for (int i = 0; i < 3; i++) 
         {
-            System.out.println("Persegi panjang ke-" + (i + 1) + ", panjang : "
-            + arrayOfPersegiPanjang[i].panjang + ", lebar  : " + arrayOfPersegiPanjang[i].lebar);
+            System.out.print("Persegi panjang ke-" + (i + 1) + ": ");
+            arrayOfPersegiPanjang[i].cetakInfo();
         }
     }
 }
