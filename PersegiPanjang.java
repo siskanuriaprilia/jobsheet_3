f17be9214eef061dc3955682f5c00679677f2011
siskanuriaprilia <aprilias878@gmail.com>
2024-02-28 13:07:24 +0700

    Percobaan 2

--- PersegiPanjang.java
+++ PersegiPanjang.java
@@ -1,25 +1,28 @@
-public class PersegiPanjang {
+import java.util.Scanner;
+    public class PersegiPanjang {
     public int panjang;
     public int lebar;
 
     public static void main(String[] args) 
     {
+        Scanner sc = new Scanner(System.in);
         PersegiPanjang[] arrayOfPersegiPanjang = new PersegiPanjang[3];
 
-        arrayOfPersegiPanjang[0] = new PersegiPanjang();
-        arrayOfPersegiPanjang[0].panjang = 110;
-        arrayOfPersegiPanjang[0].lebar = 30;
+        for (int i = 0; i < 3; i++) 
+        {
+            arrayOfPersegiPanjang[i] = new PersegiPanjang();
 
-        arrayOfPersegiPanjang[1] = new PersegiPanjang();
-        arrayOfPersegiPanjang[1].panjang = 80;
-        arrayOfPersegiPanjang[1].lebar = 40;
+            System.out.println("Persegi panjang ke-" + (i + 1));
+            System.out.print("Masukkan panjang : ");
+            arrayOfPersegiPanjang[i].panjang = sc.nextInt();
+            System.out.print("Masukkan lebar   : ");
+            arrayOfPersegiPanjang[i].lebar = sc.nextInt();
+        }
 
-        arrayOfPersegiPanjang[2] = new PersegiPanjang();
-        arrayOfPersegiPanjang[2].panjang = 100;
-        arrayOfPersegiPanjang[2].lebar = 20;
-
-        System.out.println("Persegi panjang ke-1, panjang : " + arrayOfPersegiPanjang[0].panjang + ", lebar : " + arrayOfPersegiPanjang[0].lebar);
-        System.out.println("Persegi panjang ke-2, panjang : " + arrayOfPersegiPanjang[1].panjang + ", lebar  : " + arrayOfPersegiPanjang[1].lebar);
-        System.out.println("Persegi panjang ke-3, panjang : " + arrayOfPersegiPanjang[2].panjang + ", lebar : " + arrayOfPersegiPanjang[2].lebar);
+        for (int i = 0; i < 3; i++) 
+        {
+            System.out.println("Persegi panjang ke-" + (i + 1) + ", panjang : "
+            + arrayOfPersegiPanjang[i].panjang + ", lebar  : " + arrayOfPersegiPanjang[i].lebar);
+        }
     }
 }
